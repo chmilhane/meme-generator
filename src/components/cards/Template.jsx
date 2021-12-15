e04df5946024791c21f4c743e2e99bdc37f2f8af
Milhane <70352099+chmilhane@users.noreply.github.com>
2021-12-15 21:20:37 +0100

    style(gallery): template card

--- src/components/cards/Template.jsx
+++ src/components/cards/Template.jsx
@@ -2,7 +2,10 @@ export default function Template(props) {
   return (
     <div className="flex flex-col transition ease-in-out hover:shadow-lg p-4 rounded-md bg-white border-0 border-purple-400 shadow">
       <img className="rounded-md" src={props.template.url} alt={props.template.name}></img>
-      <h1>{props.template.name}</h1>
+      <div className="flex flex-col mt-4">
+        <h1 className="font-semibold">{props.template.name}</h1>
+        <h1 className="text-gray-400">{props.template.boxCount} Boxes</h1>
+      </div>
     </div>
   );
 }
